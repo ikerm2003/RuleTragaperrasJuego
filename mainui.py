1c0d03520cddc7f2b1c7debc213783a4ea32878c
ikerm2003 <ikermoraleda@gmail.com>
2025-09-19 10:59:02 +0200

    modified:   .vscode/settings.json 	new file:   __pycache__/mainui.cpython-313.pyc 	modified:   mainui.py 	new file:   poker.py 	new file:   pyrefly.toml 	new file:   ruleta.py 	new file:   tragaperras.py

--- mainui.py
+++ mainui.py
@@ -5,29 +5,8 @@ import sys
 class MainUI(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.setWindowTitle("Simple PyQt6 App")
-        self.setGeometry(100, 100, 400, 200)
+        self.setWindowTitle("Casino de tu mama")
+        self.setGeometry(100, 100, 600, 400)
         
-        self.central_widget = QWidget()
-        self.setCentralWidget(self.central_widget)
         
-        self.layout = QVBoxLayout()
-        
-        self.label = QLabel("Enter your name:")
-        self.layout.addWidget(self.label)
-        
-        self.textbox = QLineEdit(self)
-        self.layout.addWidget(self.textbox)
-        
-        self.button = QPushButton("Greet Me", self)
-        self.button.clicked.connect(self.greet_user)
-        self.layout.addWidget(self.button)
-        
-        self.central_widget.setLayout(self.layout)
 
-    def greet_user(self):
-        name = self.textbox.text()
-        if name:
-            QMessageBox.information(self, "Greeting", f"Hello, {name}!")
-        else:
-            QMessageBox.warning(self, "Input Error", "Please enter your name.")
